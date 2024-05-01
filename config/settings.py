bbefa78b4b856650ec2f70b9f43c3229c9f6f7b4
Abror <izztullaev2001abror@gmail.com>
2024-05-01 10:31:18 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+from datetime import timedelta
+
 from dotenv import load_dotenv
 from pathlib import Path
 
