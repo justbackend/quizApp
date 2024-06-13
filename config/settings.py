6dd53118788491109be1f670e14d6fd841f27565
Abror <izztullaev2001abror@gmail.com>
2024-06-13 09:09:24 +0500

    .

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
